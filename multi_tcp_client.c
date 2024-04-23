a37c1bc16a4d689f766b72d48f03d1800d73f775
ibrahimtosun18 <ibrahim.tsn18@gmail.com>
2024-04-23 23:11:07 +0300

    Added SSL method for more secure data exchange

--- multi_tcp_client.c
+++ multi_tcp_client.c
@@ -7,18 +7,38 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/time.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 #define PORT 8080
 #define SIZE 1000
 
 int main(int argc, char *argv[]) {
+    // Initialize SSL
+    SSL_library_init();
+    OpenSSL_add_ssl_algorithms();
+    SSL_load_error_strings();
+
+    // Create an SSL context
+    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    // Load trust store
+    if (!SSL_CTX_load_verify_locations(ctx, "/home/ibrahim/Desktop/SSL certificates/ca-cert.pem", NULL))
+    {
+        ERR_print_errors_fp(stderr);
+        SSL_CTX_free(ctx);
+        exit(EXIT_FAILURE);
+    }
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (client_fd == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
-
-    char buffer[SIZE];
 
     struct sockaddr_in serverAddr;
     memset(&serverAddr, 0, sizeof(serverAddr));
@@ -31,6 +51,19 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    // Create an SSL object and attach it to the socket
+    SSL *ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, client_fd);
+
+    if (SSL_connect(ssl) != 1) {
+        ERR_print_errors_fp(stderr);
+        SSL_free(ssl);
+        close(client_fd);
+        SSL_CTX_free(ctx);
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[SIZE];
     struct timeval start, end;
     gettimeofday(&start, NULL);
 
@@ -41,14 +74,14 @@ int main(int argc, char *argv[]) {
     }
 
     // Message sending loop
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 500; i++) {
         char message[256];
         snprintf(message, sizeof(message), "Client %s: Package number %d", client_id, i);
-        if (send(client_fd, message, strlen(message), 0) < 0) {
-            perror("Send failed");
+        if (SSL_write(ssl, message, strlen(message)) <= 0) {
+            perror("SSL Send failed");
             break;
         }
-        usleep(100000); // Sleep for 10 microseconds
+        usleep(100000); // Sleep for 100 milliseconds
     }
 
     gettimeofday(&end, NULL);
@@ -56,6 +89,9 @@ int main(int argc, char *argv[]) {
     long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
     printf("Time elapsed: %ld seconds and %ld microseconds\n", seconds, micros);
 
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
     close(client_fd);
+    SSL_CTX_free(ctx);
     return 0;
 }
